c421b1ef250a6c51914545b272f0be567ab09c9a
x00Pavel <pavel.yadlsouki@gmail.com>
2023-02-22 21:26:23 +0100

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,12 +17,12 @@ release = '1.0.0'
 extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = []
-
-
+exclude_patterns = [
+    'sphinx_rtd_theme',
+]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
